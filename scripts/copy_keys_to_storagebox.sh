f526eb81587180b41bf825e937e81c4e35d72537
debauer <github@debauer.net>
2020-10-29 08:26:02 +0100

    build deployment of keys on storagebox

---
+++ scripts/copy_keys_to_storagebox.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+FOLDER="../roles/basic/users/public_keys"
+FILE="all_ssh_keys"
+
+rm "$FOLDER/$FILE"
+for filename in $(ls $FOLDER)
+do
+  if ! [ "$filename" = "$FILE" ]; then
+     cat "$FOLDER/$filename" >> $FOLDER/$FILE
+  fi
+done;
+rsync --progress -ax -e 'ssh -p23' "$FOLDER/$FILE" u220998@u220998.your-storagebox.de:./.ssh/authorized_keys
